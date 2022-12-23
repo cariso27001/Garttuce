cd7a987190f5fb5ecbf407ab05397acce79aae72
Jim Hung <cariso27001@gmail.com>
2022-12-23 11:53:17 +0800

    Fix Manifest problem and add 2nd Exp. data

--- src/Garttuce.jl
+++ src/Garttuce.jl
@@ -1,5 +1,14 @@
 module Garttuce
 
-greet() = print("Hello World!")
+using Cropbox
+
+include("atmosphere/atmosphere.jl")
+include("rhizosphere/rhizosphere.jl")
+include("phenology/phenology.jl")
+include("morphology/morphology.jl")
+include("physiology/physiology.jl")
+
+
+# include("examples/examples.jl")
 
 end # module
